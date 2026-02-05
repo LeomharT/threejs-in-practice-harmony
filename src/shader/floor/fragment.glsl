69ec1c79e2dc19e3562f42513e19bddfc2a5fcea
LeomharT <leo.liao.lzy@gmail.com>
2026-02-05 11:43:18 +0800

    feat: sin same width line

--- src/shader/floor/fragment.glsl
+++ src/shader/floor/fragment.glsl
@@ -1,3 +1,5 @@
+precision mediump float;
+
 varying vec2 vUv;
 
 uniform float uTime;
@@ -7,29 +9,24 @@ uniform float uLineWidth;
 
 void main(){
     vec2  uv        = vUv;
-    vec3  color     = vec3(0.0);
+    vec3  color     = vec3(0.02);
     float lineWidth = uLineWidth;
+    float x         = uv.x;
 
-    // uv -= uTime * 0.25;
-    // uv.y *= 5.0;
+    float wave  = sin(x * uFrequency);
+          wave *= 0.5;
+          wave  = (wave + 1.0) / 2.0;
 
-    vec2 gridUV = fract(uv);
+    float dydx      = 0.5 * cos(x * uFrequency) * uFrequency;
+    float thickness = lineWidth * sqrt(1.0 + pow(dydx, 2.0));
+    float dist      = abs(uv.y - wave) / sqrt(1.0 + dydx * dydx);
+   
+    bool c = abs(uv.y - wave) < thickness * 0.5;
+    bool c_ = uv.y > wave && uv.y < wave + lineWidth * 5.0;
 
-    float x = gridUV.x;
-
-    float wave = sin(gridUV.x * uFrequency + uTime);
-          wave = wave * 0.5 + 0.5;
-     
-    float dydx = cos(x * uFrequency + uTime) * uFrequency;
-
-    float thickness = lineWidth * sqrt(1.0 + dydx * dydx);
-
-    if(abs(gridUV.y - wave) < thickness * 0.5){
-        color = vec3(1.0);
+    if(c_ || c) {
+        color = vec3(1.0, 0.725, 0.0125);
     }
-
-    // if(girdUV.x < lineWidth) color = vec3(1.0);
-    // if(girdUV.y < lineWidth * uAspect) color = vec3(1.0);
 
     gl_FragColor = vec4(color, 1.0);
 
